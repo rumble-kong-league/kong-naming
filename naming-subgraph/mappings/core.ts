b2aa216f0e4ddbc939975f698ca13cf25045efa9
nazariyv <nazariy@inbox.ru>
2021-09-14 19:30:02 +0100

    subgraph work

--- naming-subgraph/mappings/core.ts
+++ naming-subgraph/mappings/core.ts
@@ -4,6 +4,40 @@ import {
 } from "../generated/KongNaming/KongNaming";
 import { Kong, Name, Bio } from "../generated/schema";
 
-export function handleSetName(event: SetName): void { }
+function fetchKong(): Kong { }
 
-export function handleSetBio(event: SetBio): void { }
+export function handleSetName(event: SetName): void {
+  let params = event.params;
+
+  let name = new Name(
+    event.transaction.hash
+      .toString()
+      .concat("::")
+      .concat(event.logIndex.toString())
+  );
+
+  let kong = fetchKong();
+
+  name.value = params.name.toString();
+  name.kong = kong.id;
+
+  name.save();
+}
+
+export function handleSetBio(event: SetBio): void {
+  let params = event.params;
+
+  let bio = new Bio(
+    event.transaction.hash
+      .toString()
+      .concat("::")
+      .concat(event.logIndex.toString())
+  );
+
+  let kong = fetchKong();
+
+  bio.value = params.bio;
+  bio.kong = kong.id;
+
+  bio.save();
+}
