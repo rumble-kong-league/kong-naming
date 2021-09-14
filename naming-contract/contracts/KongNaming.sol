7bd91130eda7d6723fe2c38b58a7c0d50736d99d
nazariyv <nazariy@inbox.ru>
2021-09-14 14:00:55 +0100

    kong naming contract done

--- naming-contract/contracts/KongNaming.sol
+++ naming-contract/contracts/KongNaming.sol
@@ -8,6 +8,9 @@ contract KongNaming is IKongNaming {
     mapping(uint256 => bytes32) public names;
     mapping(uint256 => string) public bios;
 
+    mapping(uint256 => bool) private nameWasSet;
+    mapping(uint256 => bool) private bioWasSet;
+
     IERC721 public constant rkl =
         IERC721(0xEf0182dc0574cd5874494a120750FD222FdB909a);
 
@@ -21,19 +24,52 @@ contract KongNaming is IKongNaming {
         beneficiary = newBeneficiary;
     }
 
-    function setName(bytes32 name, uint256 tokenID) external override {
-        // require that the caller is the owner of the kong
-        // if this is not the first time that the name is set, charge a small fee
-        names[tokenID] = name;
+    function setName(bytes32 name, uint256 tokenID) external payable override {
+        ensureOwnerOfKong(tokenID);
+        bool firstSet = nameWasSet[tokenID] == false;
+        if (firstSet) {
+            names[tokenID] = name;
+            nameWasSet[tokenID] = true;
+        } else {
+            require(
+                msg.value == 0.025 ether,
+                "KongNaming::send 0.025 ether to set name"
+            );
+            names[tokenID] = name;
+        }
     }
 
-    function setBio(string memory bio, uint256 tokenID) external override {
-        // require that the caller is the owner of the kong
-        // if this is not the first time that the bio is ser, charge a small fee
-        bios[tokenID] = bio;
+    function setBio(string memory bio, uint256 tokenID)
+        external
+        payable
+        override
+    {
+        ensureOwnerOfKong(tokenID);
+        bool firstSet = bioWasSet[tokenID] == false;
+        if (firstSet) {
+            bios[tokenID] = bio;
+            bioWasSet[tokenID] = true;
+        } else {
+            require(
+                msg.value == 0.025 ether,
+                "KongNaming::send 0.025 ether to set bio"
+            );
+            bios[tokenID] = bio;
+        }
     }
 
     function ensureAddressNotZero(address checkThis) private pure {
         require(checkThis != address(0), "KongNaming::address is zero");
+    }
+
+    function ensureOwnerOfKong(uint256 tokenID) private view {
+        address ownerOfKong = rkl.ownerOf(tokenID);
+        require(msg.sender == ownerOfKong, "KongNaming::unauthorized to name");
+    }
+
+    function withdraw() external {
+        require(msg.sender == admin, "KongNaming::unauthorized");
+        uint256 balance = address(this).balance;
+        beneficiary.transfer(balance);
     }
 }
--- naming-contract/interfaces/IKongNaming.sol
+++ naming-contract/interfaces/IKongNaming.sol
@@ -2,7 +2,7 @@
 pragma solidity =0.8.7;
 
 interface IKongNaming {
-    function setName(bytes32 name, uint256 tokenID) external;
+    function setName(bytes32 name, uint256 tokenID) external payable;
 
-    function setBio(string memory bio, uint256 tokenID) external;
+    function setBio(string memory bio, uint256 tokenID) external payable;
 }
