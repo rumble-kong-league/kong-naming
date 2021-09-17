d67e63b7359c50628d3585252c1a8308de99df0b
nazariyv <nazariy@inbox.ru>
2021-09-17 12:29:25 +0100

    skeleton for the meta update script

--- naming-update-script/main.py
+++ naming-update-script/main.py
@@ -1,29 +1,64 @@
 #!/usr/bin/env python
+from collections import dataclass
+from typing import Set
 import requests
+import dotenv
+import os
 
-SUBGRAPH_URI = "https://api.studio.thegraph.com/query/3020/kong-naming/0.0.2"
+dotenv.load_dotenv()
+
+SUBGRAPH_URI = os.environ.get("SUBGRAPH_URI", "")
+IPFS_API = os.environ.get("IPFS_API", "")
+
+
+@dataclass(frozen=True)
+class KongMeta:
+    token_id: int
+    name: str
+    bio: str
+
 
 def query_kongs(qty: int, skip: int) -> str:
-  return """{
-  kongs(
-    orderBy: id,
-    orderDirection: desc,
-    first: """ + str(qty) + """,
-    skip: """ + str(skip) + """
-  ) {
-    id
-    name {
-      value
-    }
-    bio {
-      value
-    }
-  }
-}"""
+    return (
+        """
+        {
+          kongs(
+            orderBy: id,
+            orderDirection: desc,
+            first: %s,
+            skip: %s
+          ) {
+            id
+            name {
+              value
+            }
+            bio {
+              value
+            }
+          }
+        }
+        """
+        % qty,
+        skip,
+    )
+
+
+def get_ipfs_kongs() -> Set[KongMeta]:
+    ...
+
+
+def get_naming_contract_kongs() -> Set[KongMeta]:
+    ...
+
+
+def update_metadata(
+    *, ipfs_kongs: Set[KongMeta], contract_kongs: Set[KongMeta]
+) -> None:
+    # returns all the ipfs metadata on each kong
+    ...
+
 
 def main():
-  # check the names and bios of every kong
-  # check the metadata and check the difference
-  # for the ones that you have pulled that have different names / bio
-  #    update the metadata with the new info
-  ...
+    ipfs_kongs = get_ipfs_kongs()
+    contract_kongs = get_naming_contract_kongs()
+    update_metadata(ipfs_kongs=ipfs_kongs, contract_kongs=contract_kongs)
