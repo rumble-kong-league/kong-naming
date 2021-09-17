7aeb3cfc93080fbf8bbd919b547b5a083aea4923
nazariyv <nazariy@inbox.ru>
2021-09-17 13:30:37 +0100

    pulling all the meta from the ipfs

--- naming-update-script/main.py
+++ naming-update-script/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-from collections import dataclass
+from dataclasses import dataclass
 from typing import Set
+import ipfshttpclient
 import requests
 import dotenv
+import json
 import os
 
 dotenv.load_dotenv()
@@ -13,9 +15,8 @@ IPFS_API = os.environ.get("IPFS_API", "")
 
 @dataclass(frozen=True)
 class KongMeta:
-    token_id: int
     name: str
-    bio: str
+    description: str
 
 
 def query_kongs(qty: int, skip: int) -> str:
@@ -43,8 +44,32 @@ def query_kongs(qty: int, skip: int) -> str:
     )
 
 
+def _build_kong_meta(kong_meta_json: dict) -> KongMeta:
+    return KongMeta(
+        name=kong_meta_json["name"],
+        description=kong_meta_json["description"],
+    )
+
+
 def get_ipfs_kongs() -> Set[KongMeta]:
-    ...
+    client = ipfshttpclient.connect(IPFS_API)
+
+    # this pulls all the hashes of the meta jsons
+    root_meta_dir = "QmZmghtNCGYx496Dq2U9nHuxqSbSLhNuXpFPPz6eA2urME"
+    all_meta = client.ls(root_meta_dir)["Objects"][0]["Links"]
+    all_meta = list(map(lambda x: x["Hash"], all_meta))
+    all_meta_len_pre = len(all_meta)
+
+    # this loops through all of the meta jsons and parses them
+    all_meta = set(map(lambda x: _build_kong_meta(json.loads(client.cat(x))), all_meta))
+    all_meta_len_post = len(all_meta)
+
+    assert all_meta_len_pre == all_meta_len_post, "meta len not equal"
+
+    client.close()
+    del client
+
+    return all_meta
 
 
 def get_naming_contract_kongs() -> Set[KongMeta]:
@@ -60,5 +85,10 @@ def update_metadata(
 
 def main():
     ipfs_kongs = get_ipfs_kongs()
-    contract_kongs = get_naming_contract_kongs()
-    update_metadata(ipfs_kongs=ipfs_kongs, contract_kongs=contract_kongs)
+    breakpoint()
+    # contract_kongs = get_naming_contract_kongs()
+    # update_metadata(ipfs_kongs=ipfs_kongs, contract_kongs=contract_kongs)
+
+
+if __name__ == "__main__":
+    main()
