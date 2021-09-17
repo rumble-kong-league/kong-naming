914645d2a4126e9c92d5a82421258753da44c7f2
nazariyv <nazariy@inbox.ru>
2021-09-17 17:39:55 +0100

    keeping track of the updated meta

--- naming-update-script/main.py
+++ naming-update-script/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from dataclasses import dataclass
 from typing import List
+from datetime import datetime
 import ipfshttpclient
 import requests
 import dotenv
 import json
+import time
 import os
 
 dotenv.load_dotenv()
@@ -59,8 +61,30 @@ def _sort_by_id(original: List[KongMeta]) -> KongMeta:
     return sorted(original, key=lambda k: k.token_id)
 
 
+# * saves the kongs, in case we ever need to revert
+def _save_kongs(
+    *, pre_update_kongs: List[KongMeta], post_update_kongs: List[KongMeta]
+) -> None:
+    now = datetime.now()
+    now_time = f"{now.day}-{now.month}-{now.year}::{now.hour}:{now.minute}"
+
+    pre_update_kongs = list(map(lambda x: x.__dict__, pre_update_kongs))
+    post_update_kongs = list(map(lambda x: x.__dict__, post_update_kongs))
+
+    with open(f"historical/{now_time}_pre_update_kongs.json", "w+") as f:
+        f.write(json.dumps(pre_update_kongs, indent=4))
+
+    with open(f"historical/{now_time}_post_update_kongs.json", "w+") as f:
+        f.write(json.dumps(post_update_kongs, indent=4))
+
+
 def get_ipfs_kongs() -> List[KongMeta]:
     client = ipfshttpclient.connect(IPFS_API)
+
+    def cat(cid: str) -> bytes:
+        res = client.cat(cid)
+        time.sleep(0.001)
+        return res
 
     # this pulls all the hashes of the meta jsons
     root_meta_dir = "QmZmghtNCGYx496Dq2U9nHuxqSbSLhNuXpFPPz6eA2urME"
@@ -68,8 +92,9 @@ def get_ipfs_kongs() -> List[KongMeta]:
     all_meta = list(map(lambda x: x["Hash"], all_meta))
     all_meta_len_pre = len(all_meta)
 
+    # ! this cat sometimes throws a connection error. exponential backoff for individual cats
     # this loops through all of the meta jsons and parses them
-    all_meta = set(map(lambda x: _build_kong_meta(json.loads(client.cat(x))), all_meta))
+    all_meta = set(map(lambda x: _build_kong_meta(json.loads(cat(x))), all_meta))
     all_meta_len_post = len(all_meta)
 
     assert all_meta_len_pre == all_meta_len_post, "meta len not equal"
@@ -118,8 +143,22 @@ def get_naming_contract_kongs() -> List[KongMeta]:
 def update_metadata(
     *, ipfs_kongs: List[KongMeta], contract_kongs: List[KongMeta]
 ) -> None:
-    # returns all the ipfs metadata on each kong
-    ...
+    def update_ipfs_meta(kongs: List[KongMeta]) -> None:
+        # TODO: implement
+        ...
+
+    pre_update_kongs = []
+    post_update_kongs = []
+
+    for kong in contract_kongs:
+        equivalent_ipfs_kong = ipfs_kongs[kong.token_id]
+        if hash(equivalent_ipfs_kong) != hash(kong):
+            pre_update_kongs.append(equivalent_ipfs_kong)
+            post_update_kongs.append(kong)
+
+    _save_kongs(pre_update_kongs=pre_update_kongs, post_update_kongs=post_update_kongs)
+
+    update_ipfs_meta(post_update_kongs)
 
 
 def main():
