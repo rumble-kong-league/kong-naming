76a245a377a0bfa9fe94a30fbef652be69ee8de6
nazariyv <nazariy@inbox.ru>
2021-09-25 02:09:01 +0100

    subgraph deploy

--- naming-subgraph/generated/KongNaming/KongNaming.ts
+++ naming-subgraph/generated/KongNaming/KongNaming.ts
@@ -108,6 +108,21 @@ export class KongNaming extends ethereum.SmartContract {
     return ethereum.CallResult.fromValue(value[0].toString());
   }
 
+  changePrice(): BigInt {
+    let result = super.call("changePrice", "changePrice():(uint256)", []);
+
+    return result[0].toBigInt();
+  }
+
+  try_changePrice(): ethereum.CallResult<BigInt> {
+    let result = super.tryCall("changePrice", "changePrice():(uint256)", []);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
   names(param0: BigInt): Bytes {
     let result = super.call("names", "names(uint256):(bytes32)", [
       ethereum.Value.fromUnsignedBigInt(param0)
@@ -167,12 +182,212 @@ export class ConstructorCall__Inputs {
   get newBeneficiary(): Address {
     return this._call.inputValues[1].value.toAddress();
   }
+
+  get newRkl(): Address {
+    return this._call.inputValues[2].value.toAddress();
+  }
 }
 
 export class ConstructorCall__Outputs {
   _call: ConstructorCall;
 
   constructor(call: ConstructorCall) {
+    this._call = call;
+  }
+}
+
+export class BatchSetBioCall extends ethereum.Call {
+  get inputs(): BatchSetBioCall__Inputs {
+    return new BatchSetBioCall__Inputs(this);
+  }
+
+  get outputs(): BatchSetBioCall__Outputs {
+    return new BatchSetBioCall__Outputs(this);
+  }
+}
+
+export class BatchSetBioCall__Inputs {
+  _call: BatchSetBioCall;
+
+  constructor(call: BatchSetBioCall) {
+    this._call = call;
+  }
+
+  get _bios(): Array<string> {
+    return this._call.inputValues[0].value.toStringArray();
+  }
+
+  get tokenIDs(): Array<BigInt> {
+    return this._call.inputValues[1].value.toBigIntArray();
+  }
+}
+
+export class BatchSetBioCall__Outputs {
+  _call: BatchSetBioCall;
+
+  constructor(call: BatchSetBioCall) {
+    this._call = call;
+  }
+}
+
+export class BatchSetNameCall extends ethereum.Call {
+  get inputs(): BatchSetNameCall__Inputs {
+    return new BatchSetNameCall__Inputs(this);
+  }
+
+  get outputs(): BatchSetNameCall__Outputs {
+    return new BatchSetNameCall__Outputs(this);
+  }
+}
+
+export class BatchSetNameCall__Inputs {
+  _call: BatchSetNameCall;
+
+  constructor(call: BatchSetNameCall) {
+    this._call = call;
+  }
+
+  get _names(): Array<Bytes> {
+    return this._call.inputValues[0].value.toBytesArray();
+  }
+
+  get tokenIDs(): Array<BigInt> {
+    return this._call.inputValues[1].value.toBigIntArray();
+  }
+}
+
+export class BatchSetNameCall__Outputs {
+  _call: BatchSetNameCall;
+
+  constructor(call: BatchSetNameCall) {
+    this._call = call;
+  }
+}
+
+export class BatchSetNameAndBioCall extends ethereum.Call {
+  get inputs(): BatchSetNameAndBioCall__Inputs {
+    return new BatchSetNameAndBioCall__Inputs(this);
+  }
+
+  get outputs(): BatchSetNameAndBioCall__Outputs {
+    return new BatchSetNameAndBioCall__Outputs(this);
+  }
+}
+
+export class BatchSetNameAndBioCall__Inputs {
+  _call: BatchSetNameAndBioCall;
+
+  constructor(call: BatchSetNameAndBioCall) {
+    this._call = call;
+  }
+
+  get _names(): Array<Bytes> {
+    return this._call.inputValues[0].value.toBytesArray();
+  }
+
+  get _bios(): Array<string> {
+    return this._call.inputValues[1].value.toStringArray();
+  }
+
+  get tokenIDs(): Array<BigInt> {
+    return this._call.inputValues[2].value.toBigIntArray();
+  }
+}
+
+export class BatchSetNameAndBioCall__Outputs {
+  _call: BatchSetNameAndBioCall;
+
+  constructor(call: BatchSetNameAndBioCall) {
+    this._call = call;
+  }
+}
+
+export class EditAdminCall extends ethereum.Call {
+  get inputs(): EditAdminCall__Inputs {
+    return new EditAdminCall__Inputs(this);
+  }
+
+  get outputs(): EditAdminCall__Outputs {
+    return new EditAdminCall__Outputs(this);
+  }
+}
+
+export class EditAdminCall__Inputs {
+  _call: EditAdminCall;
+
+  constructor(call: EditAdminCall) {
+    this._call = call;
+  }
+
+  get newAdmin(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+}
+
+export class EditAdminCall__Outputs {
+  _call: EditAdminCall;
+
+  constructor(call: EditAdminCall) {
+    this._call = call;
+  }
+}
+
+export class EditBeneficiaryCall extends ethereum.Call {
+  get inputs(): EditBeneficiaryCall__Inputs {
+    return new EditBeneficiaryCall__Inputs(this);
+  }
+
+  get outputs(): EditBeneficiaryCall__Outputs {
+    return new EditBeneficiaryCall__Outputs(this);
+  }
+}
+
+export class EditBeneficiaryCall__Inputs {
+  _call: EditBeneficiaryCall;
+
+  constructor(call: EditBeneficiaryCall) {
+    this._call = call;
+  }
+
+  get newBeneficiary(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+}
+
+export class EditBeneficiaryCall__Outputs {
+  _call: EditBeneficiaryCall;
+
+  constructor(call: EditBeneficiaryCall) {
+    this._call = call;
+  }
+}
+
+export class EditPriceCall extends ethereum.Call {
+  get inputs(): EditPriceCall__Inputs {
+    return new EditPriceCall__Inputs(this);
+  }
+
+  get outputs(): EditPriceCall__Outputs {
+    return new EditPriceCall__Outputs(this);
+  }
+}
+
+export class EditPriceCall__Inputs {
+  _call: EditPriceCall;
+
+  constructor(call: EditPriceCall) {
+    this._call = call;
+  }
+
+  get newChangePrice(): BigInt {
+    return this._call.inputValues[0].value.toBigInt();
+  }
+}
+
+export class EditPriceCall__Outputs {
+  _call: EditPriceCall;
+
+  constructor(call: EditPriceCall) {
     this._call = call;
   }
 }
@@ -241,6 +456,44 @@ export class SetNameCall__Outputs {
   _call: SetNameCall;
 
   constructor(call: SetNameCall) {
+    this._call = call;
+  }
+}
+
+export class SetNameAndBioCall extends ethereum.Call {
+  get inputs(): SetNameAndBioCall__Inputs {
+    return new SetNameAndBioCall__Inputs(this);
+  }
+
+  get outputs(): SetNameAndBioCall__Outputs {
+    return new SetNameAndBioCall__Outputs(this);
+  }
+}
+
+export class SetNameAndBioCall__Inputs {
+  _call: SetNameAndBioCall;
+
+  constructor(call: SetNameAndBioCall) {
+    this._call = call;
+  }
+
+  get name(): Bytes {
+    return this._call.inputValues[0].value.toBytes();
+  }
+
+  get bio(): string {
+    return this._call.inputValues[1].value.toString();
+  }
+
+  get tokenID(): BigInt {
+    return this._call.inputValues[2].value.toBigInt();
+  }
+}
+
+export class SetNameAndBioCall__Outputs {
+  _call: SetNameAndBioCall;
+
+  constructor(call: SetNameAndBioCall) {
     this._call = call;
   }
 }
